01938c2bbe9c1436e59e50b13fe0aefdcffc46cf
sz-Y-ZGWS <3070994302@qq.com>
2025-09-25 23:54:29 +0800

    计算一个数的阶乘

---
+++ 9.25_.cpp
@@ -0,0 +1,14 @@
+//¼ÆËãnµÄ½×³Ë
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+int main()
+{
+	int i = 1;
+	int n = 0;
+	int ret = 1;
+	scanf("%d", &n);
+	for (i = 1; i <= n; i++)
+		ret = ret * i;
+	printf("%d\n", ret);
+	return 0;
+}
